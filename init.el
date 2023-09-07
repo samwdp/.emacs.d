e4b9adf972e5bb0810cadb8bda0911ccd7dc5b76
Sam Precious <samwdp@gmail.com>
2023-09-07 10:41:59 +0100

    added bindings for eat

--- init.el
+++ init.el
@@ -1410,7 +1410,13 @@ re-align the table if necessary. (Necessary because org-mode has a
   :hook (eshell-mode . eat-eshell-mode)
   :hook (eshell-mode . eat-eshell-visual-command-mode))
 
-(use-package eat)
+(use-package eat
+  :config
+  (define-key eat-mode-map (kbd "C-l") 'windmove-right)
+  (define-key eat-mode-map (kbd "C-j") 'windmove-down)
+  (define-key eat-mode-map (kbd "C-k") 'windmove-up)
+  (define-key eat-mode-map (kbd "C-h") 'windmove-left)
+  )
 
 (use-package yasnippet-snippets)
 (use-package competitive-programming-snippets)
