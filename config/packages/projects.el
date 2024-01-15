7963cad969eaa27511268844af18876c2e04cb06
Sam Precious <samwdp@gmail.com>
2024-01-15 12:06:05 +0000

    some modeline bling for perspective

--- config/packages/projects.el
+++ config/packages/projects.el
@@ -14,6 +14,7 @@
   :custom
   (persp-mode-prefix-key (kbd "C-c C-p"))
   :config
+  (setq persp-modestring-dividers '(" "))
   (add-hook 'persp-created-hook (lambda () (split-window-horizontally)))
   (setq persp-nil-name "main"
         persp-modestring-short t
